be34efb5aa18a0fe38e9a16c7c7e273e03954d06
Luiz Franzon <luizfranzon05@hotmail.com>
2022-08-19 22:35:30 -0300

    ex2

---
+++ exercises/ex2.py
@@ -0,0 +1,16 @@
+a = ["Rio de Janeiro", "Natal", "Porto Alegre", "Salvador", "Manaus"]
+
+print("A primeira cidade da lista é:")
+print(a[0])
+
+print("Aqui está a segunda cidade da lista é:")
+print(a[1])
+
+print("Aqui está a terceira cidade da lista é:")
+print(a[2])
+
+print("Aqui está a quarta cidade da lista é:")
+print(a[3])
+
+print("Aqui está a quinta cidade da lista é:")
+print(a[4])
