0daef46ad364512766706c3b4fa872c887c2a930
Luiz Franzon <luizfranzon05@hotmail.com>
2022-08-22 10:28:47 -0300

    format

---
+++ studies/format.py
@@ -0,0 +1,6 @@
+name = input("Digite o seu nome: ")
+
+nameCharQuantity = len(name)
+
+print("O seu nome é {name}, e possui: {quantity} caracteres".format(name = name, quantity = nameCharQuantity))
+input("Pressione enter...")
