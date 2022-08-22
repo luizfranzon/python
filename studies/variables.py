7cac53b66adbd185a1c9db9ad71b7ff8d9a4a4a7
Luiz Franzon <luizfranzon05@hotmail.com>
2022-08-22 10:23:50 -0300

    variables

---
+++ studies/variables.py
@@ -0,0 +1,26 @@
+# Variáveis não podem começar por números.
+# Caracteres especiais não são aceitos, com exceção do underline.
+
+nome = input("Digite um funcionário: ")
+empresa = input("Digite a empresa: ")
+qtDeFuncionarios = int(input("Digite a quantidade de funcionários: "))
+mediaSalario = float(input("Digite a média do salário: "))
+
+print("---------------------------------------------")
+print(nome + " trabalha na empresa " + empresa)
+print("Possui " + str(qtDeFuncionarios) + " funcionarios")
+print("A média de salário é de: " + str(mediaSalario))
+print("---------------------------------------------")
+print(" ")
+print("Tipos de dados:")
+print("O tipo de dado da variável [nome] é: ", type(nome))
+print("O tipo de dado da variável [empresa] é: ", type(empresa))
+print("O tipo de dado da variável [qtDeFuncionario] é: ", type(qtDeFuncionarios))
+print("O tipo de dado da variável [mediaSalario] é: ", type(mediaSalario))
+
+print("---------------------------------------------")
+input("Pressione qualquer tecla...")
+
+
+
+
